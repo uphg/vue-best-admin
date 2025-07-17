db824062a4feb7ca86f348a95fee5b34018e4cfa
lvheng <1834805770@qq.com>
2025-07-17 21:09:37 +0800

    feat: 在导航栏右侧添加通知按钮和消息列表组件
    - 新增 NotificationButton 组件，支持通知消息展示
    - 集成 NPopover 实现点击弹出通知列表
    - 支持未读消息徽章显示和标记已读功能
    - 添加不同类型通知的颜色标识（info/warning/error/success）
    - 更新 layout-header 布局，使用 justify-between 排列左右内容
    - 通知列表支持滚动、时间显示和交互操作

--- src/components/layout/components/layout-header/layout-header.tsx
+++ src/components/layout/components/layout-header/layout-header.tsx
@@ -3,6 +3,7 @@ import GlobalSearch from '../global-search/global-search'
 import LayoutNavTags from '../layout-nav-tags/layout-nav-tags'
 import SidebarToggle from '../layout-sidebar/sidebar-toggle'
 import HeaderBreadcrumb from './header-breadcrumb'
+import NotificationButton from './notification-button'
 
 const LayoutHeader = defineComponent(() => {
   return () => (
@@ -10,8 +11,9 @@ const LayoutHeader = defineComponent(() => {
       <NLayoutHeader class="px-4 flex gap-2 h-[var(--header-height)] items-center" bordered>
         <SidebarToggle />
         <HeaderBreadcrumb />
-        <div class="w-50">
-          <GlobalSearch />
+        <div class="ml-auto flex items-center">
+          <GlobalSearch class="w-50" />
+          <NotificationButton />
         </div>
       </NLayoutHeader>
       <LayoutNavTags />
