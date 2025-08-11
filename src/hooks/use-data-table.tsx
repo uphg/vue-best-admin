f641b08103ec411004ebbc65ca7d7fe6af2c0912
uphg <1834805770@qq.com>
2025-08-11 17:23:13 +0800

    fix: reorder props in NDataTable for improved readability

--- src/hooks/use-data-table.tsx
+++ src/hooks/use-data-table.tsx
@@ -97,7 +97,7 @@ export function useDataTable(
   return [
     () => (
       <div>
-        <NDataTable class={rawProps.tableClass} data={data.value} columns={columns.value} loading={loading.value} {...nTableProps}>
+        <NDataTable {...nTableProps} class={rawProps.tableClass} data={data.value} columns={columns.value} loading={loading.value}>
           {slots}
         </NDataTable>
         <div class={mergeClass('mt-3 flex', pagingJustifyMap[rawProps.pagingJustify], rawProps.pagingWrapClass)}>
